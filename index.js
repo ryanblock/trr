95e6c84301bf08b6a20a6a10d14c431147c77d4d
Ryan Block <rblock@gmail.com>
2022-01-29 18:27:51 -0800

    It's got a name: `trr`! Add user config (`~/.trr.json`) with ignore, shortcuts Update lint parser, deps

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 #! /usr/bin/env node
+let { homedir } = require('os')
+let { existsSync, readFileSync } = require('fs')
 let { join } = require('path')
 let { spawn } = require('child_process')
-let update = require('update-notifier')
 let watcher = require('node-watch')
-let yargs = require('yargs')
-let { hideBin } = require('yargs/helpers')
+let minimist = require('minimist')
 let chalk = require('chalk')
+let update = require('update-notifier')
 let pkg = require(join(__dirname, 'package.json'))
+
+let prefsPath = join(homedir(), '.trr.json')
+let prefs
+if (existsSync(prefsPath)) prefs = JSON.parse(readFileSync(prefsPath))
 
 // Update
 let boxenOpts = { padding: 1, margin: 1, align: 'center', borderColor: 'green', borderStyle: 'round', dimBorder: true }
 update({ pkg, shouldNotifyInNpmScript: true }).notify({ boxenOpts })
 
 // Args
-let opts = yargs(hideBin(process.argv)).argv
+let opts = minimist(process.argv.slice(2))
 let {
   _: run,
   ignore = [],
   queue = false,
   watch = process.cwd(),
-  verbose = false
+  verbose = false,
 } = opts
 if (!run.length) {
   console.error(chalk.red('Error:'), 'Please supply a command to run')
@@ -27,16 +32,26 @@ if (!run.length) {
 }
 ignore = typeof ignore === 'string' ? [ ignore ] : ignore
 ignore.unshift('node_modules', '.git')
-let name = run.join(' ')
+if (prefs.ignore) ignore = ignore.concat(prefs.ignore)
+let input
+if (prefs?.shortcuts[run[0]]) {
+  input = prefs.shortcuts[run[0]]
+  if (run.length > 1) run.slice(1).forEach((arg, i) => {
+    input = input.replace(`$${i}`, arg)
+  })
+}
+else input = run.join(' ')
 
 // Announce
 console.log(chalk.bold(`Watching:`), watch)
-console.log(chalk.bold(`Will run:`), name)
+console.log(chalk.bold(`Will run:`), input)
 if (verbose) console.log(chalk.dim(`Ignoring: ${ignore.join(', ')}`))
 
 // Current status
 let running = false
 let queued = false
+
+if (prefs.runOnStart) go()
 
 watcher(watch, { recursive: true }, function (event, filename) {
   filename = filename.replace(watch, '').substr(1)
@@ -47,38 +62,50 @@ watcher(watch, { recursive: true }, function (event, filename) {
 
   if (verbose) console.log(`${event.charAt(0).toUpperCase() + event.substr(1)}d: ${filename}`)
 
-  function go () {
-    let start = Date.now()
-    running = true
-    console.log(chalk.bold(`Running:`), name)
-
-    let cmd = run[0]
-    let args = run.slice(1)
-    let options = {
-      cwd: process.cwd(),
-      stdio: 'inherit',
-      shell: true
-    }
-    let result = spawn(cmd, args, options)
-
-    result.on('close', code => {
-      running = false
-
-      let good = code === 0
-      let status = good ? chalk.green.bold('Success!') : chalk.red.bold('Failed :(')
-      console.log(status)
-      console.log(`  | ${name} exited ${good ? '' : 'un'}successfully with code ${code} in ${Date.now() - start}ms`)
-
-      if (queue && queued) {
-        console.log(`  | Starting queued run`)
-        queued = false
-        go()
-      }
-
-      console.log() // Break up the runs a bit
-    })
-  }
-
-  if (!running) go()
+  if (!running) go(filename)
   else if (running && queue) queued = true
 })
+
+function go (filename) {
+  let start = Date.now()
+  running = true
+  console.log(chalk.bold(`Running:`), input)
+
+  let cmd, args
+  // The inclusion of pipe shell syntax necessitates running the command as a bash abstraction
+  if (input.match(/|/) && !process.platform.startsWith('win')) {
+    cmd = '/bin/sh'
+    args = [ '-c', `'${input}'` ]
+  }
+  else {
+    let bits = input.split(' ')
+    args = bits.slice(1)
+    args = args.map(a => {
+      if (a === '{last}') a = filename
+      return a
+    })
+  }
+  let options = {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+    shell: true
+  }
+  let result = spawn(cmd, args, options)
+
+  result.on('close', code => {
+    running = false
+
+    let good = code === 0
+    let status = good ? chalk.green.bold('Success!') : chalk.red.bold('Failed :(')
+    console.log(status)
+    console.log(`  | ${running} exited ${good ? '' : 'un'}successfully with code ${code} in ${Date.now() - start}ms`)
+
+    if (queue && queued) {
+      console.log(`  | Starting queued run`)
+      queued = false
+      go()
+    }
+
+    console.log() // Break up the runs a bit
+  })
+}
